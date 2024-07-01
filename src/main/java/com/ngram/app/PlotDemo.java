3e4bb34c9a3bdb35de21bc3f1bfb7c05d8042b6b
Sudip Bose <sbose007ime.work@gmail.com>
2024-07-01 16:16:49 +0530

    Modify: Rendering chart locally using XYChart.SwingWrapper

--- src/main/java/com/ngram/app/PlotDemo.java
+++ src/main/java/com/ngram/app/PlotDemo.java
@@ -9,26 +9,26 @@ import org.knowm.xchart.XYChart;
 import static com.ngram.app.utils.Utils.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class PlotDemo {
+	
     public static void main(String[] args) {
 
         NGramMap ngm = new NGramMap(TOP_14337_WORDS_FILE, TOTAL_COUNTS_FILE);
-        ArrayList<String> words = new ArrayList<>();
+        List<String> words = new ArrayList<>();
         words.add("cat");
         words.add("dog");
 
-        ArrayList<TimeSeries> lts = new ArrayList<>();
+        List<TimeSeries> lts = new ArrayList<>();
         for (String word : words) {
-            lts.add(ngm.weightHistory(word, 1900, 1950));
+            lts.add(ngm.weightHistory(word, 2000, 2020));
         }
 
         XYChart chart = Plotter.generateTimeSeriesChart(words, lts);
-        String s = Plotter.encodeChartAsString(chart);
-        System.out.println(s);
-
-        // you can also do this to display locally:
-        // Plotter.displayChart(chart);
-
+        String base64String = Plotter.encodeChartAsString(chart);
+        System.out.println(base64String);
+        // Display chart locally:
+        Plotter.displayChart(chart);
     }
 }

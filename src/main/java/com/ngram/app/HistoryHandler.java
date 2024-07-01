9a0920e2600e40c64f85499ec5f176817c9b6207
Sudip Bose <sbose007ime.work@gmail.com>
2024-07-01 16:18:27 +0530

    Enhancement: Implementation of HistoryHandler

--- src/main/java/com/ngram/app/HistoryHandler.java
+++ src/main/java/com/ngram/app/HistoryHandler.java
@@ -2,48 +2,41 @@ package com.ngram.app;
 
 import com.ngram.app.browser.NgordnetQuery;
 import com.ngram.app.browser.NgordnetQueryHandler;
+import com.ngram.app.services.NGramMap;
 import com.ngram.app.services.TimeSeries;
 import com.ngram.app.plotting.Plotter;
 
 import org.knowm.xchart.XYChart;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
-public class DummyHistoryHandler extends NgordnetQueryHandler {
+public class HistoryHandler extends NgordnetQueryHandler {
+	
+	private NGramMap map;
+	
+	public HistoryHandler(NGramMap map) {
+		this.map = map;
+	}
+	
     @Override
     public String handle(NgordnetQuery q) {
-        System.out.println("Got query that looks like:");
-        System.out.println("Words: " + q.words());
-        System.out.println("Start Year: " + q.startYear());
-        System.out.println("End Year: " + q.endYear());
-
-        System.out.println("But I'm totally ignoring that and just plotting a parabola\n" +
-                        "and a sine wave, because your job will be to figure out how to\n" +
-                        "actually use the query data.");
-
-        TimeSeries parabola = new TimeSeries();
-        for (int i = 1400; i < 1500; i += 1) {
-            parabola.put(i, (i - 50.0) * (i - 50.0) + 3);
+    	List<String> words = q.words(); 
+    	int startYear = q.startYear();
+        int endYear = q.endYear();
+        
+        List<String> wordLabels = new ArrayList<>();
+        List<TimeSeries> popularityHistory = new ArrayList<>();
+        
+        for(String word : words) {
+        	wordLabels.add(word);
+        	popularityHistory.add(map.countHistory(word, startYear, endYear));
         }
 
-        TimeSeries sinWave = new TimeSeries();
-        for (int i = 1400; i < 1500; i += 1) {
-            sinWave.put(i, 1000 + 500 * Math.sin(i/100.0*2*Math.PI));
-        }
+        XYChart chart = Plotter.generateTimeSeriesChart(wordLabels, popularityHistory);
+        String encodedBase64Image = Plotter.encodeChartAsString(chart);
 
-        ArrayList<TimeSeries> lts = new ArrayList<>();
-        ArrayList<String> labels = new ArrayList<>();
-
-        labels.add("parabola");
-        labels.add("sine wave");
-
-        lts.add(parabola);
-        lts.add(sinWave);
-
-        XYChart chart = Plotter.generateTimeSeriesChart(labels, lts);
-        String encodedImage = Plotter.encodeChartAsString(chart);
-
-        return encodedImage;
+        return encodedBase64Image;
     }
 }

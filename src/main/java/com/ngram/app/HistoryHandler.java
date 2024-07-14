b4f8f579374cdd4aad8e52d68582ea446c91e7ac
Sudip Bose <sbose007ime.work@gmail.com>
2024-07-14 19:24:03 +0530

    Refactor: Nested Loops for clarity and maintainability

--- src/main/java/com/ngram/app/HistoryHandler.java
+++ src/main/java/com/ngram/app/HistoryHandler.java
@@ -14,9 +14,25 @@ import com.google.gson.JsonObject;
 public class HistoryHandler extends NgramQueryHandler {
 	
 	private NGramMap map;
+	private JsonArray response;
 	
 	public HistoryHandler(NGramMap map) {
 		this.map = map;
+		this.response = null;
+	}
+	
+	private void buildJsonResponse(String word, TimeSeries popularity){
+		
+		for (Map.Entry<Integer, Double> entry : popularity.entrySet()){
+            // Create a JsonObject for each entry
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("word", word);
+            jsonObject.addProperty("year", entry.getKey());
+            jsonObject.addProperty("value", entry.getValue());
+            
+            // Add the JsonObject to the array
+            this.response.add(jsonObject);
+        }
 	}
 	
     @Override
@@ -24,27 +40,12 @@ public class HistoryHandler extends NgramQueryHandler {
         List<String> words = q.words();
         int startYear = q.startYear();
         int endYear = q.endYear();
-
-        JsonArray response = new JsonArray(); 
-
+        // Create a new JsonArray object everytime to serve individual request
+        response = new JsonArray();
         for (String word : words) {
             TimeSeries weightedPopularity = map.weightHistory(word, startYear, endYear);
-
-            for (Map.Entry<Integer, Double> entry : weightedPopularity.entrySet()) {
-                Integer year = entry.getKey();
-                Double value = entry.getValue();
-
-                // Create a JsonObject for each entry
-                JsonObject jsonObject = new JsonObject();
-                jsonObject.addProperty("word", word);
-                jsonObject.addProperty("year", year);
-                jsonObject.addProperty("value", value);
-
-                // Add the JsonObject to the array
-                response.add(jsonObject);
-            }
+            buildJsonResponse(word, weightedPopularity);   
         }
-      
         return response;
     }
 }
